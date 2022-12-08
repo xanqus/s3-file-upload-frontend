8167c3c1132b71e292aa42e1d231f897506ab079
xanqus <sbk98031816@gmail.com>
2022-12-08 16:21:47 +0900

    이미지 렌더링

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -6,6 +6,7 @@ const Detail = () => {
   const navigate = useNavigate();
   const { id } = useParams();
   const [article, setArticle] = useState({});
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const getData = async () => {
       const data = await axios({
@@ -13,9 +14,12 @@ const Detail = () => {
         url: `http://localhost:8089/article/${id}`,
       });
       setArticle(data.data);
+      setLoading(false);
+      console.log(data.data);
     };
     getData();
   }, []);
+  if (loading) return <div>loading...</div>;
   return (
     <div className='flex flex-col max-w-5xl m-auto'>
       <button
@@ -30,6 +34,15 @@ const Detail = () => {
       <div className='ml-auto mr-0 mt-4'>{article?.createDate}</div>
       <div className='form-control w-full mt-8'>{article?.title}</div>
       <div className='form-control w-full mt-6'>{article?.body}</div>
+      <div className='flex w-full mt-4'>
+        {article.imageList.map((image, index) => {
+          return (
+            <div key={index} className='w-36'>
+              <img src={image.imgUrl} alt='article image' />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
