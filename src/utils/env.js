5af1645d4d8bfa784717dda1dc02ad211e4fe0c9
xanqus <sbk98031816@gmail.com>
2022-12-21 17:15:08 +0900

    backend url 변경

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL = 'http://localhost:8089/api/v1';
+// export const BACKEND_URL = 'http://localhost:8089/api/v1';
+export const BACKEND_URL = 'http://hyper-x.kr:8182/api/v1';
